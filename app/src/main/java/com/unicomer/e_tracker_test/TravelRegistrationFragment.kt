7afc49a2fac6789cd4842c2a9e1e75214721e541
raul <raul.menendez.galan@gmail.com>
2019-11-03 13:32:06 -0600

    Comit Inicial de la rama

--- app/src/main/java/com/unicomer/e_tracker_test/TravelRegistrationFragment.kt
+++ app/src/main/java/com/unicomer/e_tracker_test/TravelRegistrationFragment.kt
@@ -48,7 +48,7 @@ class TravelRegistrationFragment : Fragment() {
         listener?.onFragmentInteraction(uri)
     }
 
-    /*override fun onAttach(context: Context) {
+    override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is OnFragmentInteractionListener) {
             listener = context
@@ -60,16 +60,33 @@ class TravelRegistrationFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         listener = null
-    }*/
+    }
 
-
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
-
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TravelRegistrationFragment.
+         */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =

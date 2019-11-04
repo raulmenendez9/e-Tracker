db202050af3d0f5f370e350046d9d049d45852ee
vktor <victor.carranz@hotmail.com>
2019-11-03 23:43:06 -0600

    Added font

--- app/src/test/java/com/unicomer/e_tracker_test/ExampleUnitTest.kt
+++ app/src/test/java/com/unicomer/e_tracker_test/ExampleUnitTest.kt
@@ -7,7 +7,7 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * See [ic_cat_food documentation](http://d.android.com/tools/ic_cat_food).
  */
 class ExampleUnitTest {
     @Test

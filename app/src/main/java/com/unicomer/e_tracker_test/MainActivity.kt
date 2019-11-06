625bcf7c8dcc1dd1d5004b3ba6f0f2c802997a11
ubalbo10 <guillememo95@gmail.com>
2019-11-06 00:21:17 -0600

    agregando menu e items, imagenes dentro del home

--- app/src/main/java/com/unicomer/e_tracker_test/MainActivity.kt
+++ app/src/main/java/com/unicomer/e_tracker_test/MainActivity.kt
@@ -8,13 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        loadHome(HomeFragment())
         loadRegistrationTravel(TravelRegistrationFragment())
+    }
+
+    private fun loadHome(home: HomeFragment) {
+        val formmu = supportFragmentManager.beginTransaction()
+        formmu.replace(R.id.container_Fragment_Inicio, home).addToBackStack(null)
+        formmu.commit()
     }
 
     private fun loadRegistrationTravel(tr: TravelRegistrationFragment){
         val formmu = supportFragmentManager.beginTransaction()
-        formmu.replace(R.id.fragmentRegistrationTravel, tr).addToBackStack(null)
+        formmu.replace(R.id.container_Fragment_Inicio, tr).addToBackStack(null)
         formmu.commit()
     }
 

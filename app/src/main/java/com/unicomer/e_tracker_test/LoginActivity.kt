7a83d950411e7220673d08366d2ea600a21f8ba8
vktor <victor.carranz@hotmail.com>
2019-11-07 11:26:16 -0600

    added: splash Screen, login FragmentDialog, new launcher icon without background :dizzy_face:

--- app/src/main/java/com/unicomer/e_tracker_test/LoginActivity.kt
+++ app/src/main/java/com/unicomer/e_tracker_test/LoginActivity.kt
@@ -2,11 +2,23 @@ package com.unicomer.e_tracker_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 
 class LoginActivity : AppCompatActivity(), LoginFragment.OnFragmentInteractionListener {
-
+    var splash: View? = null //contenedor del splash
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        showSplash()
+    }
+
+
+    fun showSplash(){
+        splash = findViewById(R.id.splashlogin)
+        Handler().postDelayed({
+            splash?.visibility = View.GONE
+        },1000)
     }
 }
--- app/src/main/java/com/unicomer/e_tracker_test/LoginFragment.kt
+++ app/src/main/java/com/unicomer/e_tracker_test/LoginFragment.kt
@@ -2,12 +2,16 @@ package com.unicomer.e_tracker_test
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import com.unicomer.e_tracker_test.dialogs.LoginDialogFragment
 
 
 private const val ARG_PARAM1 = "param1"
@@ -15,9 +19,8 @@ private const val ARG_PARAM2 = "param2"
 
 
 class LoginFragment : Fragment() {
-
     var signInButton: Button? = null
-
+    var signInDialog: TextView?=null
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
@@ -33,24 +36,23 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-
-        var view = inflater.inflate(R.layout.fragment_login, container, false)
-
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
         signInButton = view?.findViewById(R.id.button_sign_in)
+        signInDialog = view?.findViewById(R.id.txt_signin_problem)
 
         signInButton?.setOnClickListener {
             Toast.makeText(view?.context, "Needs authentication!", Toast.LENGTH_SHORT).show()
-
         }
-
-
-
-
-
+        signInDialog?.setOnClickListener {
+            showDialog()
+        }
         return view
+    }
 
-
-
+    fun showDialog(){
+        val fm = activity!!.supportFragmentManager
+        val dialog = LoginDialogFragment()
+        dialog.show(fm,"DIALOG_LOGIN")
     }
 
 

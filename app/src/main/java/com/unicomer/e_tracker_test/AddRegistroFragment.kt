86048c380e5cb364fffb63b84e95a700ad150d23
Cristian Merino <cris.merino@live.com>
2019-11-15 03:14:52 -0600

    adding radioGroup functionality

--- app/src/main/java/com/unicomer/e_tracker_test/AddRegistroFragment.kt
+++ app/src/main/java/com/unicomer/e_tracker_test/AddRegistroFragment.kt
@@ -3,10 +3,17 @@ package com.unicomer.e_tracker_test
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.unicomer.e_tracker_test.Constants.ADD_RECORD_FRAGMENT
+import android.widget.Toast
+
+
 
 
 class AddRegistroFragment : Fragment() {
@@ -14,6 +21,13 @@ class AddRegistroFragment : Fragment() {
 
 
     private var listener: OnFragmentInteractionListener? = null
+
+    private var radioGroup: RadioGroup? = null
+    private var radioButtonFood: RadioButton? = null
+    private var radioButtonHotel: RadioButton? = null
+    private var radioButtonTransportation: RadioButton? = null
+    private var radioButtonOther: RadioButton? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +37,61 @@ class AddRegistroFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_registro, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_registro, container, false)
+
+
+
+        radioButtonFood = view?.findViewById(R.id.radioButton_food)
+        radioButtonTransportation = view?.findViewById(R.id.radioButton_transportation)
+        radioButtonHotel = view?.findViewById(R.id.radioButton_hotel)
+        radioButtonOther = view?.findViewById(R.id.radioButton_other)
+
+        radioGroup = view?.findViewById(R.id.radioGroup_Category)
+
+
+        radioGroup?.setOnCheckedChangeListener {group, checkedId ->
+            val radioButton = view?.findViewById<RadioButton>(checkedId)
+
+            var radioButtonSelectedId: Int = radioGroup!!.checkedRadioButtonId
+
+
+            if (radioButtonSelectedId == radioButtonFood?.id){
+                radioButtonFood?.setBackgroundResource(R.drawable.ic_category_food_selected_background_gradient)
+                radioButtonHotel?.setBackgroundResource(R.drawable.ic_category_hotel_background_gradient)
+                radioButtonTransportation?.setBackgroundResource(R.drawable.ic_category_car_background_gradient)
+                radioButtonOther?.setBackgroundResource(R.drawable.ic_category_other_background_gradient)
+
+            } else if (radioButtonSelectedId == radioButtonHotel?.id) {
+
+                radioButtonFood?.setBackgroundResource(R.drawable.ic_category_food_background_gradient)
+                radioButtonHotel?.setBackgroundResource(R.drawable.ic_category_hotel_selected_background_gradient)
+                radioButtonTransportation?.setBackgroundResource(R.drawable.ic_category_car_background_gradient)
+                radioButtonOther?.setBackgroundResource(R.drawable.ic_category_other_background_gradient)
+
+            } else if (radioButtonSelectedId == radioButtonTransportation?.id) {
+
+                radioButtonFood?.setBackgroundResource(R.drawable.ic_category_food_background_gradient)
+                radioButtonHotel?.setBackgroundResource(R.drawable.ic_category_hotel_background_gradient)
+                radioButtonTransportation?.setBackgroundResource(R.drawable.ic_category_car_selected_background_gradient)
+                radioButtonOther?.setBackgroundResource(R.drawable.ic_category_other_background_gradient)
+
+            } else if (radioButtonSelectedId == radioButtonOther?.id) {
+                radioButtonFood?.setBackgroundResource(R.drawable.ic_category_food_background_gradient)
+                radioButtonHotel?.setBackgroundResource(R.drawable.ic_category_hotel_background_gradient)
+                radioButtonTransportation?.setBackgroundResource(R.drawable.ic_category_car_background_gradient)
+                radioButtonOther?.setBackgroundResource(R.drawable.ic_category_other_selected_background_gradient)
+            }
+
+
+            //Toast.makeText(this.context, radioButton?.id.toString() + " is checked.", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this.context, radioGroup?.checkedRadioButtonId.toString() + " is checked.", Toast.LENGTH_SHORT).show()
+
+        }
+
+        radioGroup?.checkedRadioButtonId
+
+
+        return view
     }
 
     fun onButtonPressed(uri: Uri) {
@@ -44,6 +112,16 @@ class AddRegistroFragment : Fragment() {
         listener = null
     }
 
+    private fun radioButtonOnClickBackground(){
+
+
+    }
+
+    fun onCheckedChanged(radioGroup: RadioGroup) {
+        Toast.makeText(
+            this.context, "Radio button clicked " + radioGroup.checkedRadioButtonId, Toast.LENGTH_LONG).show()
+    }
+
 
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
@@ -51,7 +129,6 @@ class AddRegistroFragment : Fragment() {
     }
 
     companion object {
-
         @JvmStatic
         fun newInstance() = AddRegistroFragment()
     }
